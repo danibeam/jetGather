9c7a2659a06cab277f80ac048b03a208605561f7
Daniel Belmonte Amorós <dbelmonte@plainconcepts.com>
2024-05-01 19:54:29 +0200

    Theme + tidy up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <h1 className="text-3xl text-voodoo-500 font-bold underline">
+                Hello world!
+            </h1>
         </>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
     content: ["./src/**/*.{html,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        colors: {
+            voodoo: {
+                50: "#faf8fb",
+                100: "#f5f1f6",
+                200: "#ebe1ed",
+                300: "#dbc9de",
+                400: "#c6a8ca",
+                500: "#ac85b0",
+                600: "#8f6693",
+                700: "#765279",
+                800: "#614563",
+                900: "#573f58",
+                950: "#321f33",
+            },
+        },
     },
     plugins: [],
 };
